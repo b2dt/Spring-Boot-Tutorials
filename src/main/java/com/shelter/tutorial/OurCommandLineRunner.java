4d729b3919032d686583a34aa44a8fbc3afbc27d
Brent Thoenen <bthoenen@shelterinsurance.com>
2018-05-07 13:41:43 -0500

    Initial Commit

---
+++ src/main/java/com/shelter/tutorial/OurCommandLineRunner.java
@@ -0,0 +1,12 @@
+package com.shelter.tutorial;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OurCommandLineRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
